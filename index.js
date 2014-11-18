91d7d3d0570a466ca2be9387848ffdef731d34de
miaoever <leo.miao.ever@gmail.com>
2014-11-18 19:48:09 +0800

    complete the join and share features.

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 var etherpty = require("./lib/client");
 
-process.stdin.setRawMode(true);
-process.stdin.setEncoding('utf8');
-process.stdin.resume();
-//Prees ctrl+C to exit.
-process.stdin.on('data', function(data){
-  if (data === '\u0003')
-    process.exit();
-});
-
 var argv = require('minimist')(process.argv.slice(2));
 var action = argv._[0] || "";
 
 if (!(action === "share") && !(action === "join")) {
-  return console.log('\n>> Usage: etherpty share server_address[:port]|join url[:port] \n');
+  return console.log('>> Usage: etherpty share server_address[:port]|join url[:port] \n');
+  process.exit();
 }
 
 var url = require("url").parse(argv._[1]);
