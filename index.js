5b3597f91ee4815034a175e2168d75a6f7776f16
miaoever <leo.miao.ever@gmail.com>
2014-11-20 16:39:15 +0800

    add config file.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var etherpty = require("./lib/client");
+var config = require("./config/config.json");
 
 module.exports = etherpty;
 
@@ -6,19 +7,20 @@ var argv = require('minimist')(process.argv.slice(2));
 var action = argv._[0] || "";
 
 if (!(action === "share") && !(action === "join")) {
-  console.log('>> Usage: etherpty share server_address[:port]|join url[:port] \n');
+  console.log('>> Usage: etherpty share server_address|join url\n');
   process.exit();
 }
 
-var url = require("url").parse(argv._[1]);
+var url = require("url").parse(argv._[1] 
+                               || config.server.protocol + "://" + config.server.host + ":" + config.server.port );
 var host = url.host;
-var token = url.path.replace(/\//g, "");
 
 if (action === "share") {
   etherpty.share(host);
 } else {
+  var token =  url.path.replace(/\//g, "");
   if (!token) {
-    console.log('\n>> Usage: etherpty share server_address[:port]|join url[:port] \n');
+    console.log('\n>> Usage: etherpty share server_address|join url\n');
     process.exit();
   }
   etherpty.join(host, token);
